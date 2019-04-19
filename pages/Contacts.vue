cf1194130094903ba66bd477f42abecac4588434
Andrea Vigani <and.vigani@gmail.com>
2019-04-19 17:37:18 +0200

    added contact page with netlify form

---
+++ pages/Contacts.vue
@@ -0,0 +1,33 @@
+<template>
+<form name="contact" method="POST" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+</template>
+<script>
+export default {
+  name: 'Contacts'
+}
+</script>
+<style>
+form {
+    padding: 4rem;
+}
+input {
+    border: 1px solid blue;
+}
+button {
+    background-color: blue;
+    color: #fff;
+}
+</style>
