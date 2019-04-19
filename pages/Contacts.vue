d0755452cf0663e3d04f27e7e2c2f2ac52d6cd75
Andrea Vigani <and.vigani@gmail.com>
2019-04-19 18:01:08 +0200

    contact form antispam

--- pages/Contacts.vue
+++ pages/Contacts.vue
@@ -1,17 +1,21 @@
 <template>
-<form name="contact" method="POST" netlify>
-  <p>
-    <label>Name <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Email <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
+<form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify-recaptcha="true" data-netlify="true">
+    <p class="hidden">
+        <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+    </p>
+    <p>
+        <label>Name <input type="text" name="name" /></label>
+    </p>
+    <p>
+        <label>Email <input type="email" name="email" /></label>
+    </p>
+    <p>
+        <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <div data-netlify-recaptcha="true"></div>
+    <p>
+        <button type="submit">Send</button>
+    </p>
 </form>
 </template>
 <script>
